8aa6c7e72c77be2f159589d3dfe32ac161b623f2
jacokyle <jacokyle@mail.gvsu.edu>
2021-03-12 17:04:45 -0500

    Added names and adjusted indentation.

--- Project5/Othello.rb
+++ Project5/Othello.rb
@@ -1,166 +1,149 @@
+# frozen_string_literal: true
+
 #
 # Othello Game Class
-# Author(s): Your Name(s)
+# Author(s): Ummayair Ahmad, Kyle Jacobson
 #
 
 class Othello
+  # Constants
+  WHITE = 'W'
+  BLACK = 'B'
+  EMPTY = '-'
+  TIE = 'T'
 
-    # Constants
-    WHITE = 'W'
-    BLACK = 'B'
-    EMPTY = '-'
-    TIE = 'T'
+  # Creates getter methods for instance variables @size, @turn, @disc,
+  # @p1Disc, and @p2Disc
+  attr_reader :size, :turn, :disc, :p1Disc, :p2Disc
 
-    # Creates getter methods for instance variables @size, @turn, @disc,
-    # @p1Disc, and @p2Disc
-    attr_reader  :size, :turn, :disc, :p1Disc, :p2Disc
+  # Constructs and initializes the board of given size
+  def initialize(size, startPlayer, discColor)
+    # validate arguments
+    raise ArgumentError, 'Invalid value for board size.' if size < 4 || size > 8 || size.odd?
+    raise ArgumentError, 'Invalid value for player number.' if startPlayer < 1 || startPlayer > 2
+    raise ArgumentError, 'Invalid value for disc.' if discColor != WHITE && discColor != BLACK
 
-    # Constructs and initializes the board of given size
-    def initialize(size, startPlayer, discColor)
-        # validate arguments
-        if size < 4 || size > 8 || size % 2 != 0
-            raise ArgumentError.new('Invalid value for board size.')
-        end
-        if startPlayer < 1 || startPlayer > 2
-            raise ArgumentError.new('Invalid value for player number.')
-        end
-        if discColor != WHITE && discColor != BLACK
-            raise ArgumentError.new('Invalid value for disc.');
-        end
+    # set instance variables
+    @board = []
+    @size = size
+    @turn = startPlayer
+    @disc = discColor
 
-        # set instance variables
-        @board = []
-        @size = size
-        @turn = startPlayer
-        @disc = discColor
-
-        # set two more instance variables @p1Disc and @p2Disc
-        if @turn == 1
-            @p1Disc = @disc
-            @p2Disc = @disc == WHITE ? BLACK : WHITE
-        else
-            @p2Disc = @disc
-            @p1Disc = @disc == WHITE ? BLACK : WHITE;
-        end
-
-        # create the grid (as array of arrays)
-        @board = Array.new(@size)
-        for i in (0...@board.length)
-            @board[i] = Array.new(@size)
-            @board[i].fill(0)
-        end
-
-        # initialize the grid
-        initializeBoard()
+    # set two more instance variables @p1Disc and @p2Disc
+    if @turn == 1
+      @p1Disc = @disc
+      @p2Disc = @disc == WHITE ? BLACK : WHITE
+    else
+      @p2Disc = @disc
+      @p1Disc = @disc == WHITE ? BLACK : WHITE
     end
 
-    # Initializes the board with start configuration of discs
-    def initializeBoard()
-
-        # TO DO: COMPLETE THIS METHOD
-
+    # create the grid (as array of arrays)
+    @board = Array.new(@size)
+    (0...@board.length).each do |i|
+      @board[i] = Array.new(@size)
+      @board[i].fill(0)
     end
 
-    # Returns true if placing the disc of current player at row,col is valid;
-    # else returns false
-    def isValidMove(row, col)
-        return isValidMoveForDisc(row, col, @disc)
+    # initialize the grid
+    initializeBoard
+  end
+
+  # Initializes the board with start configuration of discs
+  def initializeBoard
+    # TO DO: COMPLETE THIS METHOD
+  end
+
+  # Returns true if placing the disc of current player at row,col is valid;
+  # else returns false
+  def isValidMove(row, col)
+    isValidMoveForDisc(row, col, @disc)
+  end
+
+  # Returns true if placing the specified disc at row,col is valid;
+  # else returns false
+  def isValidMoveForDisc(_row, _col, _disc)
+    # TO DO: COMPLETE THIS PART OF THE METHOD
+
+    # DO NOT DELETE - if control reaches this statement, then it is not a valid move
+    false
+  end
+
+  # Places the disc of current player at row,col and flips the
+  # opponent discs as needed
+  def placeDiscAt(row, col)
+    return unless isValidMove(row, col)
+
+    # place the current player's disc at row,col
+    @board[row][col] = @disc
+
+    # TO DO: COMPLETE THIS PART OF THE METHOD
+
+    # DO NOT DELETE - prepares for next turn if game is not over
+    prepareNextTurn unless isGameOver
+  end
+
+  # Sets @turn and @disc instance variables for next player
+  def prepareNextTurn
+    @turn = if @turn == 1
+              2
+            else
+              1
+            end
+    @disc = if @disc == WHITE
+              BLACK
+            else
+              WHITE
+            end
+  end
+
+  # Returns true if a valid move for current player is available;
+  # else returns false
+  def isValidMoveAvailable
+    isValidMoveAvailableForDisc(@disc)
+  end
+
+  # Returns true if a valid move for the specified disc is available;
+  # else returns false
+  def isValidMoveAvailableForDisc(_disc)
+    # TO DO: COMPLETE THIS PART OF THE METHOD
+
+    # DO NOT DELETE - if control reaches this statement, then a valid move is not available
+    false
+  end
+
+  # Returns true if the board is fully occupied with discs; else returns false
+  def isBoardFull
+    # TO DO: COMPLETE THIS PART OF THE METHOD
+
+    true
+  end
+
+  # Returns true if either the board is full or a valid move is not available
+  # for either disc
+  def isGameOver
+    isBoardFull ||
+      (!isValidMoveAvailableForDisc(WHITE) &&
+                      !isValidMoveAvailableForDisc(BLACK))
+  end
+
+  # If there is a winner, it returns Othello::WHITE or Othello::BLACK.
+  # In case of a tie, it returns Othello::TIE
+  def checkWinner
+    # TO DO: COMPLETE THIS METHOD
+  end
+
+  # Returns a string representation of the board
+  def to_s
+    str = "\n  "
+    (0...@size).each do |i|
+      str << "#{i + 1} "
     end
-
-    # Returns true if placing the specified disc at row,col is valid;
-    # else returns false
-    def isValidMoveForDisc(row, col, disc)
-
-        # TO DO: COMPLETE THIS PART OF THE METHOD
-
-        # DO NOT DELETE - if control reaches this statement, then it is not a valid move
-        return false
+    str << "\n"
+    (0...@size).each do |i|
+      str << "#{i + 1} "
+      str << "#{@board[i].join(' ')}\n"
     end
-
-    # Places the disc of current player at row,col and flips the
-    # opponent discs as needed
-    def placeDiscAt(row, col)
-        if (!isValidMove(row, col))
-            return
-        end
-
-        # place the current player's disc at row,col
-        @board[row][col] = @disc
-
-        # TO DO: COMPLETE THIS PART OF THE METHOD
-
-        # DO NOT DELETE - prepares for next turn if game is not over
-        if (!isGameOver())
-            prepareNextTurn();
-        end
-    end
-
-    # Sets @turn and @disc instance variables for next player
-    def prepareNextTurn()
-        if @turn == 1
-            @turn = 2
-        else
-            @turn = 1
-        end
-        if @disc == WHITE
-            @disc = BLACK
-        else
-            @disc = WHITE
-        end
-    end
-
-    # Returns true if a valid move for current player is available;
-    # else returns false
-    def isValidMoveAvailable()
-        isValidMoveAvailableForDisc(@disc)
-    end
-
-    # Returns true if a valid move for the specified disc is available;
-    # else returns false
-    def isValidMoveAvailableForDisc(disc)
-
-        # TO DO: COMPLETE THIS PART OF THE METHOD
-
-        # DO NOT DELETE - if control reaches this statement, then a valid move is not available
-        return false;
-    end
-
-    # Returns true if the board is fully occupied with discs; else returns false
-    def isBoardFull()
-
-        # TO DO: COMPLETE THIS PART OF THE METHOD
-
-        return true;
-    end
-
-    # Returns true if either the board is full or a valid move is not available
-    # for either disc
-    def isGameOver()
-        return isBoardFull() ||
-                    (!isValidMoveAvailableForDisc(WHITE) &&
-                                !isValidMoveAvailableForDisc(BLACK))
-    end
-
-    # If there is a winner, it returns Othello::WHITE or Othello::BLACK.
-    # In case of a tie, it returns Othello::TIE
-    def checkWinner()
-
-        # TO DO: COMPLETE THIS METHOD
-
-    end
-
-    # Returns a string representation of the board
-    def to_s()
-        str = "\n  "
-        for i in (0...@size)
-            str << (i+1).to_s + ' '
-        end
-        str << "\n";
-        for i in (0...@size)
-            str << (i+1).to_s + ' ';
-            str << @board[i].join(' ') + "\n";
-        end
-        return str;
-    end
-
+    str
+  end
 end
--- Project6/GameOfLife.rb
+++ Project6/GameOfLife.rb
@@ -1,90 +1,89 @@
+# frozen_string_literal: true
+
 #
 #  Game of Life class
 #
-#  Author(s): Your Name(s)
+#  Author(s): Ummayair Ahmad, Kyle Jacobson
 #
 class GameOfLife
+  # Creates getter methods for instance variables @rows and @cols
+  attr_reader :rows, :cols
 
-    # Creates getter methods for instance variables @rows and @cols
-    attr_reader  :rows, :cols
+  # Constructor that initializes instance variables with default values
+  def initialize
+    @grid = []
+    @rows = 0
+    @cols = 0
+  end
 
-    # Constructor that initializes instance variables with default values
-    def initialize()
-        @grid = []
-        @rows = 0
-        @cols = 0
+  # Reads data from the file, instantiates the grid, and loads the
+  # grid with data from file. Sets @grid, @rows, and
+  # @cols instance variables for later use.
+  def loadGrid(file)
+    data = IO.read(file)
+    tokens = data.strip.split(' ')
+
+    @rows = tokens.shift.to_i
+    @cols = tokens.shift.to_i
+
+    #
+    # TO DO: setup @grid as array of arrays and fill it with values from the tokens array
+    #
+  end
+
+  # Saves the current grid values to the file specified
+  def saveGrid(file)
+    data = "#{@rows} #{@cols}"
+
+    #
+    # TO DO: append the values in @grid to data
+    #
+
+    data += "\n"
+    IO.write(file, data)
+  end
+
+  # Mutates the grid to next generation
+  def mutate
+    # make a copy of grid and fill it with zeros
+    temp = Array.new(@rows)
+    (0...@rows).each do |i|
+      temp[i] = Array.new(@cols)
+      temp[i].fill(0)
     end
 
-    # Reads data from the file, instantiates the grid, and loads the
-    # grid with data from file. Sets @grid, @rows, and
-    # @cols instance variables for later use.
-    def loadGrid(file)
-        data = IO.read(file)
-        tokens = data.strip.split(' ')
+    #
+    # TO DO: set values in temp grid to next generation
+    #
 
-        @rows = tokens.shift.to_i
-        @cols = tokens.shift.to_i
+    # DO NOE DELETE: set @grid to temp grid
+    @grid = temp
+  end
 
-        #
-        # TO DO: setup @grid as array of arrays and fill it with values from the tokens array
-        #
+  # Returns the number of neighbors for cell at @grid[i][j]
+  def getNeighbors(_i, _j)
+    0
+
+    #
+    # TO DO: determine number of neighbors of cell at @grid[i][j]
+    #
+
+    # DO NOT DELETE THE LINE BELOW
+  end
+
+  # Returns a string representation of GameOfLife object
+  def to_s
+    str = "\n"
+    (0...@rows).each do |i|
+      (0...@cols).each do |j|
+        str += if (@grid[i][j]).zero?
+                 ' . '
+               else
+                 ' X '
+               end
+      end
+      str += "\n"
     end
-
-    # Saves the current grid values to the file specified
-    def saveGrid(file)
-        data = @rows.to_s + ' ' + @cols.to_s
-
-        #
-        # TO DO: append the values in @grid to data
-        #
-
-        data += "\n"
-        IO.write(file, data)
-    end
-
-    # Mutates the grid to next generation
-    def mutate()
-        # make a copy of grid and fill it with zeros
-        temp = Array.new(@rows)
-        for i in (0...@rows)
-            temp[i] = Array.new(@cols)
-            temp[i].fill(0)
-        end
-
-        #
-        # TO DO: set values in temp grid to next generation
-        # 
-
-        # DO NOE DELETE: set @grid to temp grid
-        @grid = temp
-    end
-
-    # Returns the number of neighbors for cell at @grid[i][j]
-    def getNeighbors(i, j)
-        neighbors = 0
-
-        #
-        # TO DO: determine number of neighbors of cell at @grid[i][j]
-        #
-
-        # DO NOT DELETE THE LINE BELOW
-        neighbors
-    end
-
-    # Returns a string representation of GameOfLife object
-    def to_s
-        str = "\n"
-        for i in 0...@rows
-            for j in 0...@cols
-                if @grid[i][j] == 0
-                    str += ' . '
-                else
-                    str += ' X '
-                end
-            end
-            str += "\n"
-        end
-        str
-    end
-
+    str
+  end
 end
