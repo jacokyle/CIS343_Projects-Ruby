417ff86a0914ae8e199e8bf6692691e8b697c0e5
jacokyle <jacokyle@mail.gvsu.edu>
2021-03-31 01:10:25 -0400

    Added comments and cleaned more code.

--- Project6/GameOfLife.rb
+++ Project6/GameOfLife.rb
@@ -20,33 +20,35 @@ class GameOfLife
   # grid with data from file. Sets @grid, @rows, and
   # @cols instance variables for later use.
   def loadGrid(file)
+    # Read the file into the data variable.
     data = IO.read(file)
+    # Split data using whitespace delimiter.
     tokens = data.strip.split(' ')
 
+    # Get number of rows and columns from the line read.
     @rows = tokens.shift.to_i
     @cols = tokens.shift.to_i
 
+    # Create empty two-dimensional array for grid.
     @grid = Array(@rows)
-
     for i in 0...@rows do 
       @grid[i] = Array.new(@cols)
       @grid[i].fill(0)
     end
 
-    values = 0 
-
+    # Assign values from file to the grid.
     for i in 0...@rows do
       for j in 0...@cols do
-        @grid[i][j] = tokens[values].to_i
-        values += 1
+        @grid[i][j] = tokens.shift[0].to_i
       end
     end
   end
 
-  # Saves the current grid values to the file specified
+  # Saves the current grid values to the file specified.
   def saveGrid(file)
     data = "#{@rows} #{@cols}"
 
+    # Iterates through the rows of the grid.
     for i in (0...@rows) 
 			for j in (0...@cols) 
 				data += ' '
@@ -54,32 +56,41 @@ class GameOfLife
       end
     end
 
+    # Write the data into a file.
     data += "\n"
     IO.write(file, data)
   end
 
-  # Mutates the grid to next generation
+  # Mutates the grid to next generation.
   def mutate
-    # make a copy of grid and fill it with zeros
+    # Create a copy of the grid and fill it with zeros.
     temp = Array.new(@rows)
     (0...@rows).each do |i|
       temp[i] = Array.new(@cols)
       temp[i].fill(0)
     end
 
+    # Iterates through the rows of the grid.
 		for i in 0...@rows
+      # Iterates through the columns of the grid.
 			for j in 0...@cols
+        # Obtain the number of neighbors that exist around a current cell.
         neighbors = getNeighbors(i, j)
 
+        # If the cell is currently live.
 				if (@grid[i][j] == 1) 
+          # A live cell with less than two live neighbors or more than three neighbors dies.
           if(neighbors < 2 || neighbors > 3)
 				    temp[i][j] = 0
           end
+          # A live cell with two or three live neighbors lives.
           if(neighbors == 2 || neighbors == 3)
 					  temp[i][j] = 1
           end
-        
+
+        # If the cell is currently dead.
 				else
+          #A dead cell with three live neighbors becomes live.
           if(neighbors == 3)
 					  temp[i][j] = 1
           end
@@ -87,25 +98,29 @@ class GameOfLife
 			end
 		end
 
-    # DO NOT DELETE: set @grid to temp grid
+    # Return the newly created grid for the user.
     @grid = temp
   end
 
-  # Returns the number of neighbors for cell at @grid[i][j]
+  # Returns the number of neighbors for cell at @grid[i][j].
   def getNeighbors(_i, _j)
+    # Variable for tracking the count of neighbors.
     neighbors = 0
 
+    # Iterate through the rows.
 		for x in -1..1
+      # Iterate through the columns.
 			for y in -1..1
+        # Checks if surrounding cells are live.
         col = (_i + x + @cols) % @cols
         row = (_j + y + @rows) % @rows
         
+        # If so, add to the neighbors count from the grid.
         neighbors = neighbors + @grid[col][row]
 			end
 		end
+    # Do not count yourself in neighbors.
 		neighbors -= @grid[_i][_j]
-
-    # DO NOT DELETE THE LINE BELOW
   end
 
   # Returns a string representation of GameOfLife object
