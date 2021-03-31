704d2dbcad0019830fe3d5c05f1f38cf066e4f38
jacokyle <jacokyle@mail.gvsu.edu>
2021-03-31 03:49:13 -0400

    Translated all but placeDiscAt method.

--- Project5/Othello.rb
+++ Project5/Othello.rb
@@ -51,7 +51,36 @@ class Othello
 
   # Initializes the board with start configuration of discs
   def initializeBoard
-    # TO DO: COMPLETE THIS METHOD
+    # Iterate through the rows of the board.
+    for i in (0...@size)
+      # Iterate through the columns of the board.
+      for j in (0...@size)
+        # Empty spaces will be identified with a hyphen.
+         @board[i][j] = '-';
+      end
+    end
+
+    # Switch used for initializing different board sizes.
+    case @size 
+    # 4x4 initial board layout.
+    when 4
+      @board[1][1] = 'B'
+      @board[2][1] = 'W'
+      @board[1][2] = 'W'
+      @board[2][2] = 'B'
+    # 6x6 initial board layout.
+    when 6
+      @board[2][2] = 'B'
+      @board[3][2] = 'W'
+      @board[2][3] = 'W'
+      @board[3][3] = 'B'
+    # 8x8 initial board layout.
+    when 8
+      @board[3][3] = 'B'
+      @board[4][3] = 'W'
+      @board[3][4] = 'W'
+      @board[4][4] = 'B'
+    end
   end
 
   # Returns true if placing the disc of current player at row,col is valid;
@@ -63,7 +92,45 @@ class Othello
   # Returns true if placing the specified disc at row,col is valid;
   # else returns false
   def isValidMoveForDisc(_row, _col, _disc)
-    # TO DO: COMPLETE THIS PART OF THE METHOD
+    # Variables to help with iterating through grid successfully.
+    rows = _row;
+    cols = _col;
+
+    # Iterates through the nearby positions.
+    for i in -1..1
+      for j in -1..1
+        # Increment the row and column by i and j, respectively.
+        row = rows + i;
+        col = cols + j;
+
+        # If the grid is inbounds, continue the code.
+        if (!@checkBounds)
+            next
+        end
+
+        # If the board is on the game piece, or a empty spot, continue the code.
+        if (@board[row][col] == disc || @board[row][col] == "-") 
+          next
+        end
+
+        # Iterate through positions on grid until the code is out of bounds.
+        while (@checkBounds)
+          # If the current position is empty, break from the code.
+          if (@board[row][col] == "-") 
+              break
+          end
+
+          # If the current position is the disc, return true.
+          if (@board[row][col] == disc) 
+              true
+          end
+
+          # Increment the row and column by i and j, respectively.
+          row += i;
+          col += j;
+        end
+      end
+    end
 
     # DO NOT DELETE - if control reaches this statement, then it is not a valid move
     false
@@ -74,10 +141,13 @@ class Othello
   def placeDiscAt(row, col)
     return unless isValidMove(row, col)
 
+    # Returns the player disc for next turn.
+    opponentDisc = @prepareNextTurn
+
     # place the current player's disc at row,col
     @board[row][col] = @disc
-
-    # TO DO: COMPLETE THIS PART OF THE METHOD
+ 
+    #TODO
 
     # DO NOT DELETE - prepares for next turn if game is not over
     prepareNextTurn unless isGameOver
@@ -106,7 +176,19 @@ class Othello
   # Returns true if a valid move for the specified disc is available;
   # else returns false
   def isValidMoveAvailableForDisc(_disc)
-    # TO DO: COMPLETE THIS PART OF THE METHOD
+    # Iterate through the rows of the board.
+    for i in @size do
+      # Iterate through the columns of the board.
+      for j in @size do
+        # Checks if space is currently empty.
+        if (@board[i][j] == '-')
+          # If the move is valid, return true.
+          if (@isValidMove)
+            true
+          end
+        end
+      end
+    end
 
     # DO NOT DELETE - if control reaches this statement, then a valid move is not available
     false
@@ -114,8 +196,16 @@ class Othello
 
   # Returns true if the board is fully occupied with discs; else returns false
   def isBoardFull
-    # TO DO: COMPLETE THIS PART OF THE METHOD
-
+    # Iterate through the rows of the board.
+    for i in @size do
+      # Iterate through the columns of the board.
+      for j in @size do
+        # If the board has empty spaces, return false.
+        if (@board[i][j] == '-')
+          false
+        end
+      end
+    end
     true
   end
 
@@ -130,7 +220,42 @@ class Othello
   # If there is a winner, it returns Othello::WHITE or Othello::BLACK.
   # In case of a tie, it returns Othello::TIE
   def checkWinner
-    # TO DO: COMPLETE THIS METHOD
+    # Counts the amount of points allocated to each player.
+    whitePoints = 0;
+    blackPoints = 0;
+
+    # If the game is not over, return 0.
+    if !@isGameOver
+      0
+    end
+
+    # Iterate through the rows of the board.
+    for i in @size do
+      # Iterate through the columns of the board.
+      for j in @size do
+        # If the space contains a white disc, add one point.
+        if (@board[i][j] == 'W') 
+          whitePoints += 1
+        end
+
+        # If the space contains a black disc, add one point.
+        if (@board[i][j] == 'B') 
+          blackPoints += 1
+        end
+      end
+    end
+
+    # If player "White" has more points than player "Black", return "White" as winner.
+    if (whitePoints > blackPoints) 
+        'W'
+    end
+  
+    # If player "Black" has more points than player "White", return "Black" as winner.
+    if (whitePoints < blackPoints) 
+        'B'
+    end
+    # Otherwise, declare a tie.
+    'T'
   end
 
   # Returns a string representation of the board
